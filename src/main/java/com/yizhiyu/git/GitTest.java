6027f46b474b6c53a73aeb3c7747981e235823b8
yizhiyu <3332756620@qq.com>
2023-07-13 19:52:28 +0800

    hot-fix-two commit

--- src/main/java/com/yizhiyu/git/GitTest.java
+++ src/main/java/com/yizhiyu/git/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix two commit");
     }
 }
