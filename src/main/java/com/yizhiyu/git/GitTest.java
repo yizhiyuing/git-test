dd152b0003cc24474b9b98cdaf6dff59067fc8ed
yizhiyu <3332756620@qq.com>
2023-07-13 19:34:30 +0800

    two commit

--- src/main/java/com/yizhiyu/git/GitTest.java
+++ src/main/java/com/yizhiyu/git/GitTest.java
@@ -3,5 +3,8 @@ package com.yizhiyu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
     }
 }
